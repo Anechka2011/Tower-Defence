83e02029a99fb744d3be46c87d45e7fe6810ee70
xqveer <annakostesha@yandex.ru>
2026-03-07 11:31:11 +0300

    Anya

--- main.py
+++ main.py
@@ -11,6 +11,17 @@ class Sprite:
         surface.blit(self.image, self.rect)
 
 
+class MoveSprite(Sprite):
+    def __init__(self, center, image, speed, direction):
+        super().__init__(center, image)
+        self.speed = speed
+        self.direction = direction.normalize()
+
+    def update(self):
+        vector = self.direction * self.speed
+        self.rect.move_ip(vector)
+
+
 
 WINDOW_SIZE = (800,600)
 MAX_FPS = 60
@@ -20,16 +31,34 @@ surface = window.get_surface()
 
 clock = pygame.Clock()
 
+center = WINDOW_SIZE[0]/2, WINDOW_SIZE[1]/2
+image = pygame.Surface((50,50))
+image.fill('red')
+player = Sprite(center,image)
+bullets = []
+
+
 running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.WINDOWCLOSE:
             running = False
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            image = pygame.Surface((6,6))
+            image.fill("blue")
+            center = pygame.Vector2(player.rect.center)
+            pos = pygame.Vector2(pygame.mouse.get_pos())
+            direction = pos - center
+            bullet = MoveSprite(center, image, 7, direction)
+            bullets.append(bullet)
 
-
+    for bullet in bullets:
+        bullet.update()
 
     surface.fill("white")
-    ...
+    player.render(surface)
+    for bullet in bullets:
+        bullet.render(surface)
     window.flip()
 
     clock.tick(MAX_FPS)
